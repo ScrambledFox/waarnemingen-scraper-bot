a54ced1440d66e1280932816d81d8409f79b2f7d
Joris Lodewijks <github@jorislodewijks.nl>
2024-08-06 00:32:47 +0200

    fix: remove uuid references

--- birdSighting.js
+++ birdSighting.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const uuid = require('uuid');
-
 // class that saves bird sighting containg, datetime, species, rarity, number, location, url
 module.exports = class BirdSighting {
     constructor(datetime, species, rarity, number, location, url) {
-        // this.id = uuid.v4();
         this.datetime = datetime;
         this.species = species;
         this.rarity = rarity;
