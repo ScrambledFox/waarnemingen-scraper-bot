7ab40667625eb19912f4493fb179a053df705acc
Joris Lodewijks <github@jorislodewijks.nl>
2024-08-05 22:24:04 +0200

    fix: fixed delta birds

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 // Web-scaper bot that notifies the user when a new element pops up on a table
 require('dotenv').config();
 
+// package version
+const package = require('./package.json');
+
 const axios = require('axios');
 const cheerio = require('cheerio');
 const TelegramBot = require('node-telegram-bot-api');
@@ -56,12 +59,13 @@ async function checkForNewEntries() {
 	console.log('Checking for new bird sightings...');
 	await scrapeWebsite();
 
-	// Check if there are new bird sightings
-	const newBirdSightings = currentBirdSightings.filter((birdSighting) => !previousBirdSightings.includes(birdSighting));
+	// Filter out the new bird sightings based on uuid.
+	const newBirdSightings = currentBirdSightings.filter((birdSighting) => !previousBirdSightings.some((previousBirdSighting) => previousBirdSighting.id === birdSighting.id));
 
 	// If the bot has just started, ignore the first run and only send the last bird sighting as a test
 	if (previousBirdSightings.length === 0) {
 		previousBirdSightings = currentBirdSightings;
+		currentBirdSightings = [];
 		return;
 	}
 
@@ -84,10 +88,10 @@ async function checkForNewEntries() {
 
 
 checkForNewEntries();
-// sendTelegramMessage("BirdWatcher is online and watching 👀");
+sendTelegramMessage(`BirdWatcher version ${package.version} is online and watching 👀`);
 
 // Create cron job for checking birds every 10 minutes.
-cron.schedule('*/10 * * * *', () => {
+cron.schedule('* * * * *', () => {
 	checkForNewEntries();
 });
 
@@ -96,4 +100,16 @@ const PORT = process.env.PORT || 3000;
 const app = require('express')();
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+	res.send('BirdWatcher is online and watching 👀');
+});
+
+app.get('/birds', (req, res) => {
+	res.send(previousBirdSightings);
+});
+
+app.get('/previous', (req, res) => {
+	res.send(previousBirdSightings);
 });
